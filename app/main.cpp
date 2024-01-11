a0b920e66b863b2c9a42e9414f636521cbe95856
Mehmet Ozgan <arge4@avciss.com>
2024-01-11 10:30:41 +0300

    add build info

--- app/main.cpp
+++ app/main.cpp
@@ -1,1 +1,11 @@
-auto main() -> int { return 0; }
+#include <iostream>
+
+#include "buildinfo.h"
+
+auto main() -> int {
+  std::clog << "Commit SHA: " << BuildInfo::kCommitSha << std::endl;
+  std::clog << "Timestamp : " << BuildInfo::kTimestamp << std::endl;
+  std::clog << "Version   : " << BuildInfo::kVersion << std::endl;
+
+  return 0;
+}
